05b7563100a1c8ec4ebf42f8e3bcee9b40e28158
Jan Bílek <honza@elisiondesign.cz>
2018-08-16 23:37:43 +0200

    Prepare notification components

---
+++ src/components/notifications/item.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="notify">Test</div>
+</template>
+<script>
+export default {
+  name: "v-item",
+  data() {
+    return {};
+  }
+};
+</script>
+<style scoped>
+.notify {
+  min-height: 70px;
+  width: 100%;
+  border: var(--input-border-width) solid var(--lighter-gray);
+  border-radius: 5px;
+  margin-bottom: 8px;
+}
+</style>
---
+++ src/components/notifications/notifications.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="notifications">        
+      <v-item v-for="(item, index) in notifications" :key="index"></v-item>      
+  </div> 
+</template>
+<script>
+import VItem from "./item.vue";
+export default {
+  name: "VNotifications",
+  components: {
+    VItem
+  },
+  data() {
+    return {};
+  },
+  computed: {
+    notifications() {
+      return this.$store.state.notifications.queue;
+    }
+  }
+};
+</script>
+<style scoped>
+.notifications {
+  position: fixed;
+  right: 10px;
+  bottom: 10px;
+  width: 340px;
+}
+</style>
