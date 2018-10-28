563bb837a27e3956eb4cf76b3be6905a4dab99b5
Jan Bílek <honza@elisiondesign.cz>
2018-10-28 22:54:24 +0100

    Put back example

--- src/app.vue
+++ src/app.vue
@@ -129,6 +129,65 @@ export default {
     discardChanges() {
       this.$store.dispatch("discardChanges");
     }
+  },
+  mounted() {
+    const $notify = this.$notify;
+    const $router = this.$router;
+    setTimeout(function() {
+      $notify({
+        title: "Upload error",
+        details: "There was an error while trying to upload the item",
+        iconMain: "cloud_off",
+        // iconRight: "arrow_forward",
+        delay: 0,
+        type: "error"
+      });
+    }, 3000);
+    setTimeout(function() {
+      $notify({
+        title: "Connection lost",
+        details: "You seem to be offline",
+        iconRight: "refresh",
+        iconMain: "warning",
+        delay: 0,
+        type: "warning",
+        clickCallback: () => {}
+      });
+    }, 4000);
+    setTimeout(function() {
+      $notify({
+        title: "Item created sucessfully",
+        details: "Your item was created",
+        // iconRight: "arrow_forward",
+        iconMain: "check",
+        delay: 0,
+        type: "success",
+        onclick: "sadfasdfa"
+      });
+    }, 5000);
+    setTimeout(function() {
+      $notify({
+        title: "New mention",
+        details: "Click to see the comment",
+        iconRight: "arrow_forward",
+        iconMain: "message",
+        type: "info",
+        delay: 0,
+        clickCallback: () => {
+          $router.push("/activity");
+        }
+      });
+    }, 6000);
+    setTimeout(function() {
+      $notify({
+        title: "Two factor authentication",
+        details: "Click to enable 2FA ",
+        iconRight: "edit",
+        iconMain: "lock",
+        delay: 0,
+        clickCallback: () => {}
+      });
+    }, 7000);
   }
 };
 </script>
